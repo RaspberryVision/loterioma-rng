162063096ec4d8f31098765d57e71a9340611664
Rafal Malik <rafalmalik.info@gmail.com>
2020-03-12 17:21:12 +0100

    Upgraded SF version, added working containers.

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+# Enable mod_rewrite for Apache2
+a2enmod proxy_fcgi ssl rewrite proxy proxy_balancer proxy_http proxy_ajp
+
+# Apache config for localhost
+sed -i '/Global configuration/a \
+ServerName localhost \
+' /etc/apache2/apache2.conf
+
 # INSTALL COMPOSER DEP #
-composer install
+composer update
+
+# RUN APACHE #
+apache2-foreground
