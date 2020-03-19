8caa998a8f4a49ef791f0f2733d3950612227823
RafalMalik <rafalmalik.info@gmail.com>
2020-03-19 02:20:24 +0100

    Added doc comment.

--- tests/Generator/RandomnessGeneratorTest.php
+++ tests/Generator/RandomnessGeneratorTest.php
@@ -1,11 +1,25 @@
 <?php
 
+/**
+ * Unit testing for RandomnessGenerator class.
+ *
+ * ~
+ *
+ * @category   UnitTests
+ * @package    App\Tests\Generator
+ * @author     Rafal Malik <kontakt@raspberryvision.pl>
+ * @copyright  03.2020 Raspberry Vision
+ */
+
 namespace App\Tests\Generator;
 
 use PHPUnit\Framework\TestCase;
 
 class RandomnessGeneratorTest extends TestCase
 {
+    /**
+     * Test that create new instance by constructor correctly sets field values.
+     */
     public function testCreateInstance()
     {
         $randomnessGenerator = new RandomnessGenerator(213, [0, 5], [
@@ -27,6 +41,9 @@ class RandomnessGeneratorTest extends TestCase
         ], $randomnessGenerator->getMatrix());
     }
 
+    /**
+     * Check that RandomnessGenerator class implement RandomGenerableInterface.
+     */
     public function testDoesImplementRandomGenerableInterface()
     {
         $randomnessGenerator = new RandomnessGenerator(213, [0, 5], [
@@ -38,9 +55,11 @@ class RandomnessGeneratorTest extends TestCase
         $this->assertInstanceOf(RandomGenerableInterface::class, $randomnessGenerator);
     }
 
+    /**
+     * Check if the method of setting the current state of the generator is working correctly.
+     */
     public function testSetCurrentState()
     {
-
         $matrix = [
             [-1, -1, -1],
             [-1, -1, -1],
@@ -54,6 +73,10 @@ class RandomnessGeneratorTest extends TestCase
         $this->assertEquals(23, $randomnessGenerator->getCurrentState());
     }
 
+    /**
+     * The method that resets the current state of the generator
+     * by entering a parameter can also be initialized.
+     */
     public function testReset()
     {
         $matrix = [
@@ -75,6 +98,10 @@ class RandomnessGeneratorTest extends TestCase
         $this->assertEquals(20, $randomnessGenerator->getCurrentState());
     }
 
+    /**
+     * Check that the generate method sets the values in the given
+     * matrix format and within the given range.
+     */
     public function testGenerate()
     {
         $matrix = [
